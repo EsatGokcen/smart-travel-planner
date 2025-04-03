4bfcc3c632dc4f74467d80eb3017b5d071291d24
Esat Gokcen <esatgokcen173@gmail.com>
2025-04-03 15:01:18 +0100

    initial server js file

--- server/server.js
+++ server/server.js
@@ -1,1 +1,12 @@
 const express = require('express');
+const app = express();
+
+const travelOptions = {
+    destionations: ["New York", "Paris", "Tokyo"],
+    travelModes: ["Car", "Plane", "Train"],
+    accommodations: ["Hotel", "Hostel", "Airbnb"],
+}
+
+app.get('/api/travel-options', (req, res) => {
+    res.json(travelOptions);
+});
